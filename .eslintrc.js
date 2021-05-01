8a268d9726c1fdcde30bdaf54d2beb408fe5cd4f
lillie.yang <lillie.yang@kakaocorp.com>
2021-05-01 16:34:25 +0900

    Chore: eslint, prettier 설치

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['airbnb-base', 'pretteir:prettier/recommended'],
+  plugins: 'import',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  }
+};
