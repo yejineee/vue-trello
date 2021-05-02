06cfdefe1221e0ebd5523cee52ae18c81fa7bd28
yejineee <yygeniee@gmail.com>
2021-05-03 02:07:35 +0900

    Feat: column의 리스트를 보여주는 Board 컴포넌트 추가
    columnList를 갖고, 칼럼의 리스트를 보여주는 보드 컴포넌트를 추가한다.
    이 컴포넌트는 column-form 컴포넌트에서 add-new-column이벤트를 발생시키면,
    새로운 칼럼을 추가하게 된다.

---
+++ src/components/Board.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="board">
+    <column
+      v-for="column in columnList"
+      :key="column.id"
+      :title="column.title"
+    ></column>
+    <column-form @add-new-column="onAddNewColumn"></column-form>
+  </div>
+</template>
+<script>
+import Column from './Column.vue';
+import ColumnForm from './ColumnForm.vue';
+
+export default {
+  components: {
+    Column,
+    ColumnForm
+  },
+  data() {
+    return {
+      columnList: [{ id: 1, title: 'todo' }]
+    };
+  },
+  methods: {
+    onAddNewColumn(title) {
+      this.columnList.push({ id: `new-column-${title}`, title });
+    }
+  }
+};
+</script>
+<style lang="scss">
+#board {
+  display: flex;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 95vh;
+
+  overflow-x: scroll;
+  border: 1px solid red;
+}
+</style>
