2684018335fb03ce7e598948069da8aeb9991656
yejineee <yygeniee@gmail.com>
2021-05-03 02:09:24 +0900

    Feat: 헤더 컴포넌트 추가

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header id="header">
+    {{ title }}
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "Lillie's Trello"
+    };
+  }
+};
+</script>
+<style lang="scss">
+@import 'src/style/mixin.scss';
+
+#header {
+  @include flex-center;
+  color: #170f14;
+  font: {
+    size: 1.3em;
+    weight: 800;
+  }
+  background: #cdb1c1;
+  height: 3rem;
+}
+</style>
