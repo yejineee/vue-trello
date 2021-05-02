f2f3f608ddfc6b8c4213037fb80873857f168899
yejineee <yygeniee@gmail.com>
2021-05-03 02:05:50 +0900

    Feat: 새로운 칼럼을 추가 & 유효 여부에 따른 버튼 스타일 변경
    타이틀을 입력했을 때, 타이틀 길이의 유효 여부에 따라 버튼을 활성화/비활성화시킨다.
    x버튼을 클릭하면 버튼이 닫히게 한다.
    submit버튼을 클릭하면, 새로운 칼럼이 생기고, 폼은 닫히게 한다.

--- src/components/ColumnForm.vue
+++ src/components/ColumnForm.vue
@@ -1,21 +1,61 @@
 <template>
   <div id="column-form">
-    <h1 v-if="showForm">타이틀입력하기</h1>
+    <form v-if="showForm">
+      <input
+        id="column-form__input"
+        v-model.trim="title"
+        placeholder="Enter column title..."
+        :minlength="minTitle"
+        :maxlength="maxTitle"
+      />
+      <button
+        class="column-form__submit"
+        :class="{ active: isValidTitle }"
+        type="button"
+        @click="submitNewTitle"
+      >
+        Add List
+      </button>
+      <button id="column-form__cancel" type="button" @click="toggleShowForm">
+        X
+      </button>
+    </form>
     <button v-else id="column-form__button" @click="toggleShowForm">
       + Add another list
     </button>
   </div>
 </template>
 <script>
+import { MAX_TITLE_LENGTH, MIN_TITLE_LENGTH } from '../constants/title';
+
 export default {
   data() {
     return {
-      showForm: false
+      showForm: false,
+      title: '',
+      maxTitle: MAX_TITLE_LENGTH,
+      minTitle: MIN_TITLE_LENGTH
     };
+  },
+  computed: {
+    isValidTitle() {
+      return (
+        this.title.length >= MIN_TITLE_LENGTH &&
+        this.title.length <= MAX_TITLE_LENGTH
+      );
+    }
   },
   methods: {
     toggleShowForm() {
       this.showForm = this.showForm !== true;
+    },
+    submitNewTitle() {
+      this.$emit('add-new-column', this.title);
+      this.resetData();
+    },
+    resetData() {
+      this.showForm = false;
+      this.title = '';
     }
   }
 };
@@ -24,19 +64,31 @@ export default {
 @use "sass:color";
 @import 'src/style/mixin.scss';
 @import 'src/style/variable.scss';
+$padding: 0.5rem 0;
+
+.column-form__submit {
+  color: gray;
+}
+
+.active {
+  background: green;
+  color: white;
+  &:hover {
+    background: color.scale(green, $lightness: 10%);
+  }
+}
 
 #column-form {
   @include round-box;
   @include column-base;
-
-  background: #f9f9f9;
 }
+
 #column-form__button {
   @include round-box;
-  @include column-title-base;
 
   width: 100%;
   border: lightgray;
+  padding: $padding;
   background: color.scale($column-back, $lightness: 50%);
   font: {
     size: 1.2em;
@@ -44,9 +96,11 @@ export default {
   }
 
   cursor: pointer;
-
-  &:hover {
-    background: color.scale($column-back, $lightness: 80%);
-  }
+}
+#column-form__input {
+  @include column-title-base;
+  @include column-base;
+  width: 100%;
+  padding: $padding;
 }
 </style>
