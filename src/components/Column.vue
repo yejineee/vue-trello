24ea8ef01fce9ac0fadac317af70c62d309291b6
yejineee <yygeniee@gmail.com>
2021-05-03 02:08:54 +0900

    Feat: column 컴포넌트 추가
    title을 props로 받아서 보여주는 column 컴포넌트를 추가한다.

---
+++ src/components/Column.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="column">
+    <div class="column__title">
+      {{ title }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import 'src/style/mixin.scss';
+@import 'src/style/variable.scss';
+
+.column {
+  @include round-box;
+  @include column-base;
+  background: $column-back;
+}
+.column__title {
+  @include round-box;
+  @include flex-center;
+  @include column-title-base;
+  font: {
+    size: 1.2em;
+    weight: 500;
+  }
+  background: $column-back;
+}
+</style>
