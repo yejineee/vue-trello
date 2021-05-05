5c50c1e3ab11a0016f4dd7f72dfe0ac72c19aae6
yejineee <yygeniee@gmail.com>
2021-05-05 13:46:41 +0900

    Markup: 불필요한 top, left삭제 및 border 선 삭제

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -39,12 +39,10 @@ export default {
 <style lang="scss">
 #board {
   display: flex;
-  top: 0;
-  left: 0;
   width: 100%;
   height: 95vh;
+  padding: 1rem;
 
   overflow-x: scroll;
-  border: 1px solid red;
 }
 </style>
