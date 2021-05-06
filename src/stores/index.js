b3d0135b8640607c6e5d3a2ab0e4a5cba66e3873
yejineee <yygeniee@gmail.com>
2021-05-07 01:35:25 +0900

    Feat: store 이름 상수화 및 적용

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import userModule from './user';
 import columnModule from './column';
+import { USER_STORE_NAME } from './user/constants';
+import { COLUMN_STORE_NAME } from './column/constants';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    userModule,
-    columnModule
+    [USER_STORE_NAME]: userModule,
+    [COLUMN_STORE_NAME]: columnModule
   }
 });
 
