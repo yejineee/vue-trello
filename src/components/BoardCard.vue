5c7b8649d92f99c4f338659b7cf81c7f145c3031
yejineee <yygeniee@gmail.com>
2021-05-10 23:55:41 +0900

    Feat: props로 board 추가 및 validator 추가

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <li id="board-card">{{ board.title }}</li>
+</template>
+<script>
+export default {
+  props: {
+    board: {
+      type: Object,
+      required: true,
+      validator({ id, title }) {
+        return typeof id === 'string' && typeof title === 'string';
+      }
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import 'src/style/mixin.scss';
+</style>
