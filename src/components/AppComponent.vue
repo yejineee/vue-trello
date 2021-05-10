061b978c8dfceec0ebb2fccae65d60063ea29009
yejineee <yygeniee@gmail.com>
2021-05-10 09:46:42 +0900

    Feat: login, main, NotFound page routing 추가

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -1,25 +1,3 @@
 <template>
-  <div id="app">
-    <main-header></main-header>
-    <board></board>
-  </div>
+  <router-view></router-view>
 </template>
-<script>
-import { mapActions } from 'vuex';
-import { FETCH_USER, USER_STORE_NAME } from 'src/stores/user/constants';
-import Board from './Board.vue';
-import MainHeader from './MainHeader.vue';
-
-export default {
-  components: {
-    MainHeader,
-    Board
-  },
-  created() {
-    this.fetchUser();
-  },
-  methods: {
-    ...mapActions(USER_STORE_NAME, { fetchUser: FETCH_USER })
-  }
-};
-</script>
---
+++ src/components/Page404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    404 Page
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+<style lang="scss"></style>
